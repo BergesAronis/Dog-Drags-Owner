7c7da731124d6fb4b1fb68462ec0a7c6b139ed4b
Berges <iraniber@hotmail.com>
2018-04-25 09:39:00 -0400

    Add ragdoll scene

---
+++ Dog Drag Owner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveForce = 365f;
+    public float maxSpeed = 5f;
+
+
+    Rigidbody2D playerRigidbody;
+
+    public float moveSpeed;
+	// Use this for initialization
+	void Start ()
+    {
+        playerRigidbody = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        if (horizontal * playerRigidbody.velocity.x < maxSpeed)
+        {
+            playerRigidbody.AddForce(Vector2.right * horizontal * moveForce);
+        }
+
+        if (Mathf.Abs(playerRigidbody.velocity.x) > maxSpeed)
+        {
+            playerRigidbody.velocity = new Vector2(Mathf.Sign(playerRigidbody.velocity.x) * maxSpeed, playerRigidbody.velocity.y);
+        }
+
+        if (horizontal == 0)
+        {
+            playerRigidbody.velocity = new Vector2(0, playerRigidbody.velocity.y);
+        }
+
+        if (vertical * playerRigidbody.velocity.y < maxSpeed)
+        {
+            playerRigidbody.AddForce(Vector2.up * vertical * moveForce);
+        }
+
+        if (Mathf.Abs(playerRigidbody.velocity.y) > maxSpeed)
+        {
+            playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, Mathf.Sign(playerRigidbody.velocity.y) * maxSpeed);
+        }
+
+        if (vertical == 0)
+        {
+            playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, 0);
+        }
+
+    }
+}
